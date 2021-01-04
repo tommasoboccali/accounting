603c7227d2a77d82134db120cec919a788206172
Tommaso Boccali <Tommaso.Boccali@cern.ch>
2021-01-04 10:34:36 +0100

    change end date

--- update-accounting-saldo.sh
+++
@@ -1,9 +0,0 @@
-#scp tboccali@login.marconi.cineca.it:/marconi_work/Pra18_4658/accounting/accounting-saldo.out .
-scp boccali@bastion.cnaf.infn.it:/data/CMS/boccali/Cineca_Accounting/accounting-saldo.out .
-scp boccali@bastion.cnaf.infn.it:/data/CMS/boccali/Cineca_Accounting/jobs\*.out .
-scp boccali@bastion.cnaf.infn.it:/data/CMS/boccali/Cineca_Accounting/statscms .
-cat statscms |sort|uniq -c |sort -n|tail -n 5
-python3 accounting-saldo.py
-tail -n 10 jobs.out
-
-
--- update-accounting.sh
+++
@@ -1,4 +0,0 @@
-#scp tboccali@login.marconi.cineca.it:/marconi_work/Pra18_4658/accounting/accounting-\*.gz .
-scp boccali@bastion.cnaf.infn.it:/data/CMS/boccali/Cineca_Accounting/accounting-\*.gz .
-python3 accounting.py
-
